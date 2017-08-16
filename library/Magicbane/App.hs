7c417504590fc40ecd37a61e3c3a80640eb4d8dd
Greg V <greg@unrelenting.technology>
2017-08-16 17:14:48 +0300

    Use the Handler newtype wrapper, fix #4

--- library/Magicbane/App.hs
+++ library/Magicbane/App.hs
@@ -9,7 +9,7 @@ module Magicbane.App (
 , module Magicbane.App
 ) where
 
-import           ClassyPrelude
+import           ClassyPrelude hiding (Handler)
 import           Control.Monad.Trans.Except as X
 import           Control.Monad.Except as X (MonadError, throwError)
 import           Data.Proxy as X
@@ -17,28 +17,28 @@ import           Data.Has as X
 import           Servant as X hiding (And)
 
 newtype MagicbaneApp β α = MagicbaneApp {
-  unMagicbaneApp ∷ ReaderT β (ExceptT ServantErr IO) α
+  unMagicbaneApp ∷ ReaderT β Handler α
 } deriving (Functor, Applicative, Monad, MonadIO, MonadBase IO,
             MonadThrow, MonadCatch, MonadError ServantErr, MonadReader β)
 
 instance MonadBaseControl IO (MagicbaneApp β) where
-  type StM (MagicbaneApp β) α = StM (ReaderT β (ExceptT ServantErr IO)) α
+  type StM (MagicbaneApp β) α = StM (ReaderT β Handler) α
   liftBaseWith f = MagicbaneApp $ liftBaseWith $ \x → f $ x . unMagicbaneApp
   restoreM       = MagicbaneApp . restoreM
 
-runMagicbaneExcept ∷ β → MagicbaneApp β α → ExceptT ServantErr IO α
-runMagicbaneExcept ctx a = ExceptT $ liftIO $ runExceptT $ runReaderT (unMagicbaneApp a) ctx
+runMagicbaneHandler ∷ β → MagicbaneApp β α → Handler α
+runMagicbaneHandler ctx a = Handler $ ExceptT $ liftIO $ runHandler $ runReaderT (unMagicbaneApp a) ctx
 
-magicbaneToExcept ∷ β → MagicbaneApp β :~> ExceptT ServantErr IO
+magicbaneToHandler ∷ β → MagicbaneApp β :~> Handler
 #if MIN_VERSION_servant_server(0,10,0)
-magicbaneToExcept ctx = NT $ runMagicbaneExcept ctx
+magicbaneToHandler ctx = NT $ runMagicbaneHandler ctx
 #else
-magicbaneToExcept ctx = Nat $ runMagicbaneExcept ctx
+magicbaneToHandler ctx = Nat $ runMagicbaneHandler ctx
 #endif
 
 -- | Constructs a WAI application from an API definition, a Servant context (used for auth mainly), the app context and the actual action handlers.
 magicbaneApp api sctx ctx actions = serveWithContext api sctx $ srv ctx
-  where srv c = enter (magicbaneToExcept c) actions
+  where srv c = enter (magicbaneToHandler c) actions
 
 -- | Gets a value of any type from the context.
 askObj ∷ (Has β α, MonadReader α μ) ⇒ μ β
