2dc952669bdcb2a7378bae2d479453b7d1f83782
Theodore Lief Gannon <tanuki@gmail.com>
2018-07-12 11:56:16 -0700

    Add applyHeaders, removeHeaders; use in postForm, postJson

--- library/Magicbane/HTTPClient.hs
+++ library/Magicbane/HTTPClient.hs
@@ -1,5 +1,5 @@
 {-# OPTIONS_GHC -fno-warn-orphans #-}
-{-# LANGUAGE OverloadedStrings, UnicodeSyntax, FlexibleContexts, FlexibleInstances, UndecidableInstances, ConstraintKinds #-}
+{-# LANGUAGE NoImplicitPrelude, OverloadedStrings, UnicodeSyntax, FlexibleContexts, FlexibleInstances, UndecidableInstances, ConstraintKinds #-}
 
 -- | Provides an HTTP(S) client via http-client(-tls) in a Magicbane app context.
 --   Also provides a simple composable interface for making arbitrary requests, based on http-client-conduit.
@@ -9,7 +9,9 @@ module Magicbane.HTTPClient (
 , module X
 ) where
 
-import           Control.Monad.Reader
+import           RIO
+import qualified RIO.Map as M
+import qualified RIO.Set as S
 import           Control.Monad.Catch as X (MonadCatch)
 import           Control.Monad.Trans.Except as X (ExceptT (..), runExceptT)
 import qualified Control.Monad.Trans.Except as MTE
@@ -60,17 +62,19 @@ anyStatus req = return $ setRequestIgnoreStatus req
 
 -- | Sets a x-www-form-urlencoded form as the request body (also sets the content-type).
 postForm ∷ (MonadHTTP ψ μ) ⇒ [(Text, Text)] → Request → ExceptT Text μ Request
-postForm form req =
-  return req { method = "POST"
-             , requestHeaders = [ (hContentType, "application/x-www-form-urlencoded; charset=utf-8") ]
-             , requestBody = RequestBodyBS $ writeForm form }
+postForm form req = return
+  $ req { method = "POST"
+        , requestBody = RequestBodyBS $ writeForm form }
+    & applyHeaders
+        [ (hContentType, "application/x-www-form-urlencoded; charset=utf-8") ]
 
 -- | Sets a JSON value as the request body (via ToJSON; also sets the content-type).
 postJson :: (MonadHTTP ψ μ, ToJSON α) ⇒ α → Request → ExceptT Text μ Request
-postJson body req =
-  return req { method = "POST"
-             , requestHeaders = [ (hContentType, "application/json; charset=utf-8") ]
-             , requestBody = RequestBodyBS . L.toStrict . encode $ body }
+postJson body req = return
+  $ req { method = "POST"
+        , requestBody = RequestBodyBS . L.toStrict . encode $ body }
+    & applyHeaders
+        [ (hContentType, "application/json; charset=utf-8") ]
 
 -- | Performs the request, using a given function to read the body. This is what all other performWith functions are based on.
 performWithFn ∷ (MonadHTTP ψ μ, MonadCatch μ) ⇒ (ConduitM ι ByteString μ () → ConduitT () Void μ ρ) → Request → ExceptT Text μ (Response ρ)
@@ -87,3 +91,19 @@ performWithVoid = performWithFn (const $ return ())
 -- | Performs the request, reading the body into a lazy ByteString.
 performWithBytes ∷ (MonadHTTP ψ μ, MonadCatch μ) ⇒ Request → ExceptT Text μ (Response L.ByteString)
 performWithBytes = performWithFn (.| C.sinkLazy)
+
+applyHeaders :: RequestHeaders → Request → Request
+applyHeaders headers req =
+  req { requestHeaders = updated }
+  where
+    updated = M.toList $ new `M.union` old
+    new = M.fromList headers
+    old = M.fromList $ requestHeaders req
+
+removeHeaders :: [HeaderName] → Request → Request
+removeHeaders headerNames req =
+  req { requestHeaders = updated }
+  where
+    updated = M.toList $ old `M.withoutKeys` keysToRemove
+    keysToRemove = S.fromList headerNames
+    old = M.fromList $ requestHeaders req
