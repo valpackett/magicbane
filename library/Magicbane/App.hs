a0ff2547dd38c6cb666b6562ca700cc643a928e0
Adam Harris <adam@omnified.io>
2020-11-10 12:23:24 +0100

    Added HasContextEntry constraint to allow usage with Servant 0.18

--- library/Magicbane/App.hs
+++ library/Magicbane/App.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE NoImplicitPrelude, UnicodeSyntax, TypeApplications, ScopedTypeVariables, CPP #-}
+{-# LANGUAGE NoImplicitPrelude, UnicodeSyntax, TypeApplications, ScopedTypeVariables, CPP, TypeOperators, FlexibleContexts #-}
 
 -- | Extends Servant with context, based on RIO.
 --   The context should be a tuple of all your moudles and configs and stuff, so that the Data.Has module would let you access these items by type.
@@ -18,6 +18,6 @@ runMagicbaneHandler ∷ β → RIO β α → Servant.Handler α
 runMagicbaneHandler ctx a = Servant.Handler $ ExceptT $ try $ runReaderT (unRIO a) ctx
 
 -- | Constructs a WAI application from an API definition, a Servant context (used for auth mainly), the app context and the actual action handlers.
-magicbaneApp ∷ forall β χ ψ. (HasServer χ ψ) ⇒ Proxy χ → Context ψ → β → ServerT χ (RIO β) → Application
+magicbaneApp ∷ forall β χ ψ. (HasServer χ ψ, HasContextEntry (ψ .++ DefaultErrorFormatters) ErrorFormatters) ⇒ Proxy χ → Context ψ → β → ServerT χ (RIO β) → Application
 magicbaneApp api sctx ctx actions = serveWithContext api sctx $ srv ctx
   where srv c = hoistServerWithContext api (Proxy @ψ) (runMagicbaneHandler c) actions
